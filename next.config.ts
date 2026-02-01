339ab3a974a39dde22cd6865149f4864a6304228
Francesco Maiomascio <francesco@maiomascio.dev>
2026-02-01 10:49:48 +0100

    chore: set turbopack root

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,17 @@
 import type { NextConfig } from "next";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const root = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  turbopack: {
+    root,
   },
 
   experimental: {
